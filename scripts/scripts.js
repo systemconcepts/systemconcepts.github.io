0a5d9472ce7e92fd96dda668d9091b3eafe738d0
terminaidan <aidanftwalker@gmail.com>
2016-10-25 13:23:19 +0100

    Added sml_ images Resize font
    Added new images class to change on resize
    Made the front change size on resize
    Made the jQuery DRY

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,26 @@
-$(window).on('resize', function() {
-  if($(window).width() < 768) {
-    $('#mainBody').addClass('container-fluid');
-    $('#mainBody').removeClass('container');
-  }else{
-    $('#mainBody').addClass('container');
-    $('#mainBody').removeClass('container-fluid');
-  }
-})
-
-$(window).ready(function() {
-  if($(window).width() < 768) {
-    $('#mainBody').addClass('container-fluid');
-    $('#mainBody').removeClass('container');
-  }else{
-    $('#mainBody').addClass('container');
-    $('#mainBody').removeClass('container-fluid');
-  }
-})
-
 $(document).ready(function() {
   $('.case').each(function(fadeInDiv){
     $(this).delay(fadeInDiv * 400).fadeIn(1000);
   });
 })
 
+$(document).ready(changeClass, changeFontSize);
+$(window).on('resize', changeClass, changeFontSize);
 
-// $(document).ready(function() {
-//    $('.word').each(function(fadeInDiv){
-//      $(this).delay(fadeInDiv * 200).fadeIn(200);
-//    });
-// });
+function changeClass() {
+  if($(window).width() < 768) {
+    $('#mainBody').addClass('container-fluid');
+    $('#mainBody').removeClass('container');
+  }else{
+    $('#mainBody').addClass('container');
+    $('#mainBody').removeClass('container-fluid');
+  }
+}
+
+function changeFontSize() {
+  if($(window).width() <= 575) {
+    $('.title').css('font-size', '1.25em');
+  }else{
+    $('.title').css('font-size', '1.626em');
+  }
+}
