8c091e2138049a61a4a667753cb17c01c6bfde5e
terminaidan <aidanftwalker@gmail.com>
2016-11-10 14:38:35 +0000

    Addedd code to stop the app jumping out of the mobile app experience

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,27 @@
+// Listen for ALL links at the top level of the document. For
+// testing purposes, we're not going to worry about LOCAL vs.
+// EXTERNAL links - we'll just demonstrate the feature.
+$( document ).on(
+    "click",
+    "a",
+    function( event ){
+
+        // Stop the default behavior of the browser, which
+        // is to change the URL of the page.
+        event.preventDefault();
+
+        // Manually change the location of the page to stay in
+        // "Standalone" mode and change the URL at the same time.
+        location.href = $( event.target ).attr( "href" );
+
+    }
+);
+
 $(window).on('load', function(){
     loaded();
-
 });
-
-// $(document).ready(function(){
-//
-// });
 
 function loaded(){
   // $('#loader').addClass('hidden');
   $('#loader').fadeOut('slow')
 }
-
-// $('#step-by-step').click(function(){
-//   showPop();
-// })
-//
-// $('.close-btn').click(function(){
-//   hidePop();
-// })
-//
-// function showPop(){
-//   $('.pop-over').animate({top: '0'}, 1000 , "easeOutQuint");
-//   $('.pop-over').toggleClass('hidden');
-// }
-//
-// function hidePop(){
-//   $('.pop-over').animate({top: '100vh'}, 1000 , "easeInQuint", function(){
-//       $('.pop-over').toggleClass('hidden')
-//   });
-// }
