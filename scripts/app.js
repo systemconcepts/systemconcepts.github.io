421d8abe749e2daeb40589bef2594f2c5542c26c
terminaidan <aidanftwalker@gmail.com>
2016-11-10 12:48:02 +0000

    Loader test

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,31 @@
-$('#step-by-step').click(function(){
-  showPop();
-})
-
-$('.close-btn').click(function(){
-  hidePop();
-})
-
-function showPop(){
-  $('.pop-over').animate({top: '0'}, 1000 , "easeOutQuint");
-  $('.pop-over').toggleClass('hidden');
-}
-
-function hidePop(){
-  $('.pop-over').animate({top: '100vh'}, 1000 , "easeInQuint", function(){
-      $('.pop-over').toggleClass('hidden')
-  });
-}
-
-
-// external js: flickity.pkgd.js
-
-// var $carousel = $('.carousel').flickity({
-//   imagesLoaded: true,
-//   percentPosition: false,
+// $(window).on('load', function(){
+//     $("div#loader").fadeOut();
+//
 // });
+
+$(document).ready(function(){
+    loaded();
+});
+
+function loaded(){
+  $('#loader').addClass('hidden');
+}
+
+// $('#step-by-step').click(function(){
+//   showPop();
+// })
 //
-// var $imgs = $carousel.find('.carousel-cell img');
-// // get transform property
-// var docStyle = document.documentElement.style;
-// var transformProp = typeof docStyle.transform == 'string' ?
-//   'transform' : 'WebkitTransform';
-// // get Flickity instance
-// var flkty = $carousel.data('flickity');
+// $('.close-btn').click(function(){
+//   hidePop();
+// })
 //
-// $carousel.on( 'scroll.flickity', function() {
-//   flkty.slides.forEach( function( slide, i ) {
-//     var img = $imgs[i];
-//     var x = ( slide.target + flkty.x ) * -1/3;
-//     img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+// function showPop(){
+//   $('.pop-over').animate({top: '0'}, 1000 , "easeOutQuint");
+//   $('.pop-over').toggleClass('hidden');
+// }
+//
+// function hidePop(){
+//   $('.pop-over').animate({top: '100vh'}, 1000 , "easeInQuint", function(){
+//       $('.pop-over').toggleClass('hidden')
 //   });
-// });
+// }
